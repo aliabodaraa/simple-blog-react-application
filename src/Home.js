a0a2f05b70936d11b4f950544aad6bd9f33fb0cf
Ali Abodaraa <abodaraaali50@gmail.com>
2023-03-03 19:34:13 +0200

    useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,9 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import BlogList from './BlogList';
+
 const Home = () => {
+    useEffect(()=>console.log("call when render the page in case we reload or any reactive value change"));
+
     const [blogs,setBlogs]=useState([
         {title:"title 1",body:"body 1",author:"author 1",id:1},
         {title:"title 2",body:"body 2",author:"author 2",id:2},
@@ -8,10 +11,9 @@ const Home = () => {
         {title:"title 4",body:"body 4",author:"author 4",id:4}
     ]);
     const handleDelete=(id) => {
-        const newBlogs=blogs.filter(blog => blog.id!=id);
+        const newBlogs=blogs.filter(blog => blog.id!==id);
         setBlogs(newBlogs);
     };
-    
     return ( 
         <div className = "home" >
             <BlogList blogs={blogs} title={"allBlogs"} handleDelete={handleDelete}/>
@@ -22,3 +24,10 @@ const Home = () => {
 }
 
 export default Home;
+// useState hook use when the reactive variables changes that lead to rerendering the page to updates the value of the variable
+// useEffect that will run every time there is a re-render so once initially when the component first load but thereafter the data changes,like when you delete a blog from array then the state is changed and that will trigger a re-render and on that re-render again that trigger the function that pass to useEffect(fun)
+//     useEffect(()=>console.log("call when render the page in case we reload or any reactive value change"));
+
+// you can make the useEffect triggers only when the component first load via add an empty array as a second param that represents acual dependencies
+//  useEffect(()=>console.log("call when render the page in case we reload or any reactive value change"),[]);
+//  note : any reactive value you added to dependencies array, the useEffect is going to watch this value and if it changes it will run the function
