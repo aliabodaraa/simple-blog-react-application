17a9056cc75ba990c009842677e9db77053e63f6
Ali Abodaraa <abodaraaali50@gmail.com>
2023-03-03 18:24:16 +0200

    props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+
 import Navbar from './Navbar';
 import Home from './Home';
+
+const handleClick=(e)=>{
+	console.log(e.target);
+}
+const handleClickWithParam=(name,e)=>{
+	console.log(name,e.target);
+}
 function App() {
+
   return (
     <div className="App">
         <Navbar/>
+        <button onClick={handleClick}>click me1</button>
+        <button onClick={(e)=>handleClickWithParam("param",e) }>click me2</button>
+
         <div className="content">
           <Home/>
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
 const Home = () => {
+    const [blogs,setBlogs]=useState([
+        {title:"title 1",body:"body 1",author:"author 1",id:1},
+        {title:"title 2",body:"body 2",author:"author 2",id:2},
+        {title:"title 3",body:"body 3",author:"author 3",id:3},
+        {title:"title 4",body:"body 4",author:"author 4",id:4}
+    ]);
+    const handleDelete=(id) => {
+        const newBlogs=blogs.filter(blog => blog.id!=id);
+        setBlogs(newBlogs);
+    };
+    
     return ( 
-        <div className = "hom" >
-            <h2>Homepage</h2>
+        <div className = "home" >
+            <BlogList blogs={blogs} title={"allBlogs"} handleDelete={handleDelete}/>
+            <hr />
+            <BlogList blogs={blogs.filter((blog)=> blog.author!=='author 1'&& blog.author!=='author 2')} title={"Filtered Blogs"}/>
         </div>
     );
 }
